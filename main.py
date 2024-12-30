d294d7198bbffa2799f5e34658e537c2c54854cc
jawnR <shawn@que9.co>
2024-12-30 16:40:47 -0500

    added player

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import pygame
 import constants
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    player = Player(constants.SCREEN_WIDTH // 2, constants.SCREEN_HEIGHT // 2)
+
     while True:
-        # Add this event handling block
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
                 
-        # Modified fill call
         screen.fill((0, 0, 0))  # Black color
+        player.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
